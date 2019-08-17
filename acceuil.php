795b70e379ffdbb3ece7066bc7f44e6993cdcbe2
Moustapha Ndiaye <ndiayemoustapha2394@gmail.com>
2019-08-17 19:15:48 +0200

    Creation du dossier cloud et ecriture du script pour lister les fichiers du cloud dans acceuil.php

--- acceuil.php
+++ acceuil.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<?php
+$adresse="./cloud"; //Adresse du dossier sans oublier le / à la fin.
+$dossier=Opendir($adresse); //Ouverture du dossier.
+?> 
 <html>
 <head>
 	<title></title>
@@ -12,6 +16,29 @@
 <main>
 <section>
 	<h1>Infos general</h1>
+	<article>
+	<table>
+		<tr>
+			<th>cloud</th>
+			<th>fonction</th>
+		</tr>
+		
+			<?php 
+			while ($Fichier = readdir($dossier)) //On affiche les fichiers les uns après les autres.
+{
+     //Maintenant, on affiche les fichiers sous forme de liens vers les fichiers 
+     //(NB : Les lien sont en target="_blank" ce qui signifie qu'ils ouvrirons une nouvelle page dans votre navigateur !).
+	if ($Fichier!=="." && $Fichier!="..") {
+	     echo '<tr>
+     <td><a href='.$adresse.$Fichier.' target="_blank">'.$Fichier.'</a><td>
+     </tr>';
+ }
+
+}
+closedir($dossier);
+			?>
+	</table>
+	</article>
 </section>
 </main>
 
