4637209fffca259afbcdb1bff11c6b8474b5301e
Moustapha Ndiaye <ndiayemoustapha2394@gmail.com>
2019-08-18 23:20:51 +0200

    Taille des fichiers et largeur tableau fixer.

--- acceuil.php
+++ acceuil.php
@@ -6,9 +6,11 @@ $dossier1=Opendir($adresse1);
 $dossier2=Opendir($adresse2);
 $list1 = array();
 $list2 = array();
+$size1= array();
 while (false !==($fichier1=readdir($dossier1))) {
 	if ($fichier1!=="." && $fichier1!=="..") {
 		$list1[]=$fichier1;
+		$size1[]=filesize($adresse1.$fichier1);
 	}
 }
 while (false !==($fichier2=readdir($dossier2))) {
@@ -19,6 +21,37 @@ while (false !==($fichier2=readdir($dossier2))) {
 if (count($list1)<count($list2)) {
 	$supp=count($list2);
 }else{$supp=count($list1);
+}
+function GetSizeName($octet)
+{
+    // Array contenant les differents unités 
+    $unite = array('octet','ko','mo','go');
+    
+    if ($octet < 1000) // octet
+    {
+        return $octet.$unite[0];
+    }
+    else 
+    {
+        if ($octet < 1000000) // ko
+        {
+            $ko = round($octet/1024,2);
+            return $ko.$unite[1];
+        }
+        else // Mo ou Go 
+        {
+            if ($octet < 1000000000) // Mo 
+            {
+                $mo = round($octet/(1024*1024),2);
+                return $mo.$unite[2];
+            }
+            else // Go 
+            {
+                $go = round($octet/(1024*1024*1024),2);
+                return $go.$unite[3];    
+            }
+        }
+    }
 }
 ?>
 <html>
@@ -59,7 +92,7 @@ if (count($list1)<count($list2)) {
              	}
              	echo '<tr>
              	<td>'.($i+1).'</td>
-             	<td>'.$list1[$i].'</td><td>oiu</td><td>oiu</td>
+             	<td>'.$list1[$i].'</td><td>oiu</td><td>'.GetSizeName($size1[$i]).'</td>
              	<td>'.$list2[$i].'</td>
              	</tr>';
              }
