97400df369447eed315985f8ad8601b278e26175
Moustapha Ndiaye <ndiayemoustapha2394@gmail.com>
2019-08-30 17:58:02 +0200

    Modifications dans le css.

---
+++ maths.php
@@ -0,0 +1,18 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Section Mathematique</title>
+	<link rel="stylesheet" type="text/css" href="maths.css">
+	<link rel="stylesheet" type="text/css" href="nav.css">
+	<link rel="stylesheet" type="text/css" href="header.css">
+</head>
+<body>
+<?php 
+include 'header.inc.php';
+include 'nav.inc.php';
+?>
+</body>
+</html>
